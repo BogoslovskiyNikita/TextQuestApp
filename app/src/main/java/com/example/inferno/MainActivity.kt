0d281fc0b87989c4a1a96e72cdaca6168f4e0c5b
Nikita <kartawyh1337@yandex.ru>
2020-07-10 06:35:49 +0300

    added button functional, renamed icons

--- app/src/main/java/com/example/inferno/MainActivity.kt
+++ app/src/main/java/com/example/inferno/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.inferno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        StartGame.setOnClickListener() {
+            val intent = Intent(applicationContext, StoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        Exit.setOnClickListener() {
+            finish()
+            System.exit(0)
+        }
     }
 }
