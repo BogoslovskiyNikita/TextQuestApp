ffbccc0320d99c74dc29bf8c3944e89d17a0500d
Anna <anna.mikhailovna305@gmail.com>
2020-07-11 13:21:32 +0300

    RockPaperScissors || added

---
+++ app/src/main/java/com/example/inferno/RockPaperScissors.kt
@@ -0,0 +1,12 @@
+package com.example.inferno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RockPaperScissors : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rock_paper_scissors)
+    }
+}
