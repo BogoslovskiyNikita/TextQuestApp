5c5d052af4d4378e70b3b59b628ce95b1c7dd61e
MakK <Ooogrob@yandex.ru>
2020-07-12 15:30:25 +0300

    added: EndStoryDesign, good and bad ending

---
+++ app/src/main/java/com/example/inferno/EndStoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.inferno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EndStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_story)
+    }
+}
