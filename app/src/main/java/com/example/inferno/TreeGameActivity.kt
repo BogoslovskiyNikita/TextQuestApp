f323937f8b10b0a0073dae5a9491bacec46f2884
Anna <anna.mikhailovna305@gmail.com>
2020-07-13 11:38:56 +0300

    TreeGame || added

---
+++ app/src/main/java/com/example/inferno/TreeGameActivity.kt
@@ -0,0 +1,14 @@
+package com.example.inferno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TreeGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tree_game)
+
+        /**/
+    }
+}
