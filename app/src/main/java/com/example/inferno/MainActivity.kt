d27da88be303136ad30226aecae79cd8650da5c6
Dina3308 <57904472+Dina3308@users.noreply.github.com>
2020-07-14 23:01:38 +0300

    add music

--- app/src/main/java/com/example/inferno/MainActivity.kt
+++ app/src/main/java/com/example/inferno/MainActivity.kt
@@ -11,19 +11,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        startService(Intent(this, MyService::class.java))
         StartGame.setOnClickListener() {
+            stopService(Intent(this, MyService::class.java))
             val intent = Intent(applicationContext, StoryActivity::class.java)
             startActivity(intent)
             finish()
         }
 
         button.setOnClickListener() {
+            stopService(Intent(this, MyService::class.java))
             val intent = Intent(applicationContext, RockPaperScissorsActivity::class.java)
             startActivity(intent)
         }
 
         Exit.setOnClickListener() {
+            stopService(Intent(this, MyService::class.java))
             finish()
         }
 
