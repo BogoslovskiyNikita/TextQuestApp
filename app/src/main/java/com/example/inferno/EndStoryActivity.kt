2dc2b445e5deebc71b8022785fb2e32f7329f023
MakK <Ooogrob@yandex.ru>
2020-07-14 20:32:03 +0300

    added: switch on EndStoryActivity in to ways

--- app/src/main/java/com/example/inferno/EndStoryActivity.kt
+++ app/src/main/java/com/example/inferno/EndStoryActivity.kt
@@ -1,11 +1,25 @@
 package com.example.inferno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class EndStoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_end_story)
+
+        val end = intent.extras
+        if (end != null) {
+            if (end["badEnd"] != null) {
+                setContentView(R.layout.bad_end_of_the_story)
+            } else {
+                setContentView(R.layout.activity_end_story)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/inferno/StoryActivity.kt
+++ app/src/main/java/com/example/inferno/StoryActivity.kt
@@ -4,12 +4,28 @@ import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_story.*
 
 class StoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story)
+
+        intent = Intent(this, EndStoryActivity::class.java)
+
+        badEnd.setOnClickListener() {
+            intent.putExtra("badEnd", 76)
+            startActivity(intent)
+        }
+
+        goodEnd.setOnClickListener() {
+            intent.putExtra("goodEnd", 0)
+            startActivity(intent)
+        }
+
     }
 
     override fun onBackPressed() {
