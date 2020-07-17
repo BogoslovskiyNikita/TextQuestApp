3641854fc42794effff049f47bd3dcd72e86c48a
NikitaRomanchuk <61383494+NikitaRomanchuk@users.noreply.github.com>
2020-07-17 20:26:36 +0300

    Update BloodyKnife.kt

--- app/src/main/java/com/example/inferno/player/BloodyKnife.kt
+++ app/src/main/java/com/example/inferno/player/BloodyKnife.kt
@@ -1,6 +1,6 @@
 package com.example.inferno.player
 
 class BloodyKnife : Item {
-    override val name = "BloodyKnife"
+    override var name = "BloodyKnife"
     override val pic = "blody_knife_icon"
 }
